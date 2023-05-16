bd08c6b9430be9fc231eb4aedf1dfd4c729fa8d5
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-05-16 17:34:05 +0000

    favorites combined all by accessing type done and delete method done

--- src/models.py
+++ src/models.py
@@ -77,4 +77,23 @@ class Vehicles(db.Model):
             "passengers":self.passengers
         } 
 
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    favorite_id = db.Column(db.Integer, unique=False, nullable=False)
+    favorite_type = db.Column(db.String(256), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Favorite {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_if": self.user_id,
+            "favorite_id":self.favorite_id,
+            "favorite_type" : self.favorite_type
+            # do not serialize the password, its a security breach
+        }
+
     
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
-from models import db, User, Character, Vehicles, Planet
+from models import db, User, Character, Vehicles, Planet, Favorites
 from utils import APIException
 
 api = Blueprint('api', __name__)
@@ -63,10 +63,64 @@ def add_PLANET():
     db.session.commit()
     return 'PLANET WAS CREATED', 200
 
+
+
+#DELETE METHOD
 @api.route('/planet/<int:id>', methods=['DELETE'])
 def delete_PLANET(id):
     planet = Planet.query.get_or_404(id)
     db.session.delete(planet)
     db.session.commit()
     return 'PLANET WAS DELETED', 200
+
+@api.route('/vehicle/<int:id>', methods=['DELETE'])
+def delete_VEHICLE(id):
+    vehicle = Vehicles.query.get_or_404(id)
+    db.session.delete(vehicle)
+    db.session.commit()
+    return 'VEHICLE WAS DELETED', 200
+
+@api.route('/character/<int:id>', methods=['DELETE'])
+def delete_CHARACTER(id):
+    character = Character.query.get_or_404(id)
+    db.session.delete(character)
+    db.session.commit()
+    return 'CHARACTER WAS DELETED', 200
+
+#FAVORITES
+@api.route('/users/favorites', methods=['GET'])
+def get_ALLFAVORITES(id):
+    favorites = get_UserFav(id)
+    favorites =[favorite.serialized() for favorite in favorites]
+    return jsonify(favorites=favorites)
+
+
+def get_UserFav(id):
+    favorites = Favorites.query.all()
+    if favorites is None :
+        return "THERE ARE NONE"
+    else:
+        fav = []
+        for favorite in favorites:
+            if favorite.user_id == id :
+                fav.apend(favorite)
+        return fav 
+    
+@api.route('/addfavorites', methods = ['POST'])
+def add_Fav():
+    request_body= request.get_json()
+    favorite = Favorites(
+        user_id = request_body['user_id'],
+        favorite_id = request_body['favorite_id'],
+        favorite_type = request_body['favorite_type']
+    )
+    db.session.add(favorite)
+    db.session.commit
+    return "SUCCESFULL"
+@api.route('/deletefavorites/<int:id>', methods = ['DELETE'])
+def delete_Fav(id):
+    Favorites.query.filter_by(id=id).delete()
+    db.session.commit
+    return "SUCCESFULL DELETE"
+
 
