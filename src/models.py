9bf569a13fd352759ee4135f6ee1eb96b8ec6d72
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-05-19 00:43:46 +0000

    favorite type updated to connect

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", back_populates="user")
 
 
     def __repr__(self):
@@ -25,6 +26,7 @@ class Character(db.Model):
     name = db.Column(db.String(20), unique=True, nullable=False)
     haircolor = db.Column(db.String(20), unique=True, nullable=False)
     eyecolor = db.Column(db.String(20), unique=True, nullable=False)
+    favorites = db.relationship("Favorites", back_populates="character")
 
 
     def __repr__(self):
@@ -43,6 +45,7 @@ class Character(db.Model):
 class Planet(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(20), unique=True, nullable=False)
+     favorites = db.relationship("Favorites", back_populates="planet")
      climate = db.Column(db.String)
      gravity = db.Column(db.String)
 
@@ -63,6 +66,7 @@ class Vehicles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
     manufacturer = db.Column(db.String(50), unique=True, nullable=False)
+    favorites = db.relationship("Favorites", back_populates="vehicles")
     length = db.Column(db.Numeric (4,2))
     passengers = db.Column(db.Integer)
 
@@ -79,9 +83,19 @@ class Vehicles(db.Model):
 
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True, nullable=False)
-    favorite_id = db.Column(db.Integer, unique=False, nullable=False)
-    favorite_type = db.Column(db.String(256), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True, nullable=False)
+    #favorite_id = db.Column(db.Integer, unique=False, nullable=False)
+    #favorite_type = db.Column(db.String(256), unique=False, nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'), nullable=True)
+
+    user = db.relationship("User", back_populates="favorites")
+    character = db.relationship("Character", back_populates="favorites")
+    planet = db.relationship("Planet", back_populates="favorites")
+    vehicles = db.relationship("Vehicles", back_populates="favorites")
+
+   
 
     def __repr__(self):
         return f'<Favorite {self.id}>'
@@ -90,8 +104,11 @@ class Favorites(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "favorite_id": self.favorite_id,
-            "favorite_type": self.favorite_type
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle_id,
+            #"favorite_id": self.favorite_id,
+            #"favorite_type": self.favorite_type
             # do not serialize the password, it's a security breach
         }
 
